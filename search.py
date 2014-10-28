9ac00e024191555f4d171ed00c2c773d5aaf72bc
Brent Baumgartner <brent@brentbaum.com>
2014-10-28 13:33:05 -0400

    Refactor search.py!

--- search.py
+++ search.py
@@ -2,44 +2,39 @@ import requests
 import json
 import time
 import sys
+
+user_agent = {'User-agent': 'super cool UVA bot by /u/AD_Curry'}
 def getSubData(subreddit, keyword):
-    nextpost = ""
-    datastr = '{"data":['
+    next_id = ""
+    data_str = '{"data":['
     search_json = ""
-    #file = open("data/data.json", "a")
-    #file.write('{"data":[')
-    #file.close()
-    for x in range(0,10000000):
-            r = requests.get("http://www.reddit.com/r/" + subreddit +"/search.json?q=" + keyword + "&sort=new&restrict_sr=on&limit=100")
-            file = open(subreddit + "-" + keyword + ".json", "w")
+    r = requests.get("http://www.reddit.com/r/" + subreddit +"/search.json?q=" + keyword + "&sort=new&restrict_sr=on&limit=100", headers = user_agent)
+    # Loop over the first 100 pages of results
+    for page in range(0,100):
             search_json = r.text
-            print search_json
-            file.write(search_json)
             data = json.loads(search_json)
-            for x in range(0, len(data["data"]["children"]) - 1):
-                    apple = data["data"]["children"][x]["data"]["title"]
-                    created = str(data["data"]["children"][x]["data"]["created"])
-                    score = str(data["data"]["children"][x]["data"]["score"])
-                    if '\u200e' not in apple:
-                            apple = apple.replace('"','\\"')
-                            datastr += '{\n\t"title": "' + apple + '",\n'
-                            datastr += '{\n\n"score": "' + score + '",\n'
-                            datastr += '\t"created": ' + created + "\n},\n"
-                            #file.write("\n")
-                            #file.write('\t"title"' + ' : ' + '"')
-                            #file.write(apple);
-                            #file.write('",')
-                            #file.write("\n")
-                            #file.write('\t"created"' + ' : ')
-                            #file.write(str(data["data"]["children"][x]["data"]["created"]))
-                            #file.write("\n")
-                            #file.write("},")
-                            #file.write("\n")
-            print "Processed page " + str(x)
+            #Loop over all the posts on a page
+            for post_index in range(0, len(data["data"]["children"]) - 1):
+                    post = data["data"]["children"][post_index]["data"]["title"]
+                    post = post.replace('"','\\"')
+                    created = str(data["data"]["children"][post_index]["data"]["created"])
+                    score = str(data["data"]["children"][post_index]["data"]["score"])
+                    data_str += '{\n\t"title": "' + post + '",\n'
+                    data_str += '{\n\n"score": "' + score + '",\n'
+                    data_str += '\t"created": ' + created + "\n},\n"
+            print "Processed page " + str(page)
             if(data["data"]["after"]):
-                    nextpost = data["data"]["after"]
+                    next_id = data["data"]["after"]
             time.sleep(1/15)
-            r = requests.get("http://www.reddit.com/r/" + subreddit +"/search.json?q=" + keyword + "&sort=new&restrict_sr=on&limit=100&after=" + nextpost)
+            r = requests.get("http://www.reddit.com/r/" + subreddit +"/search.json?q=" + keyword + "&sort=new&restrict_sr=on&limit=100&after=" + next_id, headers = user_agent)
+
+    data_str += "]}"
+
+    file = open(("./data/" + subreddit.encode('utf-8') + "-" + keyword.encode('utf-8') + ".json"), "w")
+    file.write(data_str.encode('utf-8'))
+    file.close()
+
+    
 if __name__ == "__main__":
         if(len(sys.argv) < 3):
                 print "Must supply command line arguments!"
